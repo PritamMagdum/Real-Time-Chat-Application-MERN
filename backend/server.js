f387e660277f1f21da923aa04217d36fb2fa30ec
Pritam Magdum <pritammagdum24@gmail.com>
2024-02-13 22:57:16 +0530

    Added Frontend Setup and Important Packages

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,29 @@
-const express = require('express')
+const express = require('express');
+const chats = require('./data/data');
+const dotenv = require('dotenv')
 
 const app = express();
-const PORT = 5000;
+dotenv.config();
 
-app.listen(PORT, console.log("server started on 5000 PORT"));
+app.get('/', (req, res) => {
+    res.send("Hey we on 5000 PORT");
+
+    // res.send(data);
+})
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+})
+
+app.get('/api/chat/:id', (req, res) => {
+    // console.log(req.params.id);
+    const singleChat = chats.find((e) => e._id === req.params.id)
+    if (singleChat) {
+        res.send(singleChat);
+    } else {
+        res.send("No Matches Found");
+    }
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`server started on ${PORT}`));
