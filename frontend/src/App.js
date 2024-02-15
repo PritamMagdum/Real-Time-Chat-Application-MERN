6c701e80a6d5928c65e573571d226ef6d2c5e35c
Pritam Magdum <pritammagdum24@gmail.com>
2024-02-15 21:05:37 +0530

    Added Signup Component To Create a User Account

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import ChatPage from './components/ChatPage';
+import "./App.css"
 
 function App() {
   return (
