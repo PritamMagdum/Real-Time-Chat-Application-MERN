97053594cbf9548799da078907b883371c862733
Pritam Magdum <pritammagdum24@gmail.com>
2024-02-16 14:14:32 +0530

    Added User Login Functionality

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -1,14 +1,65 @@
-import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack, useToast } from '@chakra-ui/react'
 import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
     const [show, setShow] = useState(false)
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
+    const [loading, setLoading] = useState(false)
 
+    const toast = useToast()
+    const navigate = useNavigate()
     const handleClick = () => setShow(!show)
 
-    const submitHandler = () => { }
+    const submitHandler = async () => {
+        setLoading(true);
+        if (!email || !password) {
+            toast({
+                title: "Please Fill all the Fields",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: 'bottom'
+            });
+            setLoading(false);
+            return;
+        }
+
+        try {
+            const config = {
+                headers: {
+                    "Content-type": "application/json"
+                }
+            };
+
+            const { data } = await axios.post("/api/user/login", { email, password }, config);
+
+            toast({
+                title: "Login Successful",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+                position: 'bottom'
+            });
+
+            localStorage.setItem("userInfo", JSON.stringify(data));
+            setLoading(false);
+            navigate("/chats");
+
+        } catch (error) {
+            toast({
+                title: "Error Occured!",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: 'bottom'
+            });
+            setLoading(false);
+        }
+    };
+
     return (
         <VStack>
             <FormControl id='email' isRequired>
@@ -34,6 +85,7 @@ const Login = () => {
                 width="100%"
                 style={{ marginTop: 15 }}
                 onClick={submitHandler}
+                isLoading={loading}
             >
                 Login
             </Button>
